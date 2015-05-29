dabbc40db873df9f7bd533f73026d64d63715f63
Denny Dai <i@daihan.cn>
2015-05-30 01:07:20 +0800

    remove some plugins

--- plugins/exec.php
+++
@@ -1,6 +0,0 @@
-<?php
-//PluginList("exec", "Execute");
-if (preg_match("/^".TAG."exec (.*)$/", $_GET['text'], $matches) and $_GET['from'] == 'Denny_Dai') {
-	$msg = file_get_contents("http://1.vps.dennx.com/?exec=".urlencode($matches[1]));
-	$BOT->msg($from,$msg);
-}
