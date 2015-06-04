72a36e516469ef3405b36be13f61dfeadfeab022
Denny Dai <i@daihan.cn>
2015-06-04 17:25:51 +0800

    fix plugins ["trans","tts"]

--- plugins/trans.php
+++ plugins/trans.php
@@ -1,16 +1,20 @@
 <?php
-//PluginList("trans", "translate");
+PluginList("trans", "translate");
 if (preg_match("/^".TAG."trans (.*)$/", $_GET['text'], $matches)) {
 	$matches = explode(" ", $matches[1], 2);
 	if (!isset($matches[1])) {
-		$to = "zh";
-		$text = $matches[0];
+		$to = "en";
+		$text = urlencode($matches[0]);
+	}elseif (isset($matches[1]) and strlen($matches[0]) != 2) {
+		$to = "en";
+		$text = urlencode($matches[0].$matches[1]);
 	}else{
 		$to = $matches[0];
-		$text = $matches[1];
+		$text = urlencode($matches[1]);
 	}
-	$trans = file_get_contents("http://tool.dennx.com/translate/?text=".$text."&to=".$to);
-	$msg = "Result: ".$trans;
+	$trans = file_get_contents('http://api.microsofttranslator.com/v2/ajax.svc/TranslateArray2?appId=%22TKT68kjRgkUbVtIKst6Vo0Hxnb6g2f0K3tUMyn1gZ7nc*%22&to="'.$to.'"&options={}&texts=["'.$text.'"]');
+	preg_match('/"TranslatedText":"([^"]+)/i', $trans, $trans_result);
+	$msg = "Result: ".$trans_result[1];
 	$BOT->exec("msg ".$from." ".escapeString($msg));
 
 }
--- plugins/tts.php
+++ plugins/tts.php
@@ -1,7 +1,7 @@
 <?php
 PluginList("tts", "Text To Speech");
 if (preg_match("/^".TAG."tts (.*)$/", $_GET['text'], $matches)) {
-	$mp3 = file_get_contents("http://tts.baidu.com/text2audio?lan=zh&ie=UTF-8&text=".$matches[1]);
+	$mp3 = file_get_contents("http://tts.baidu.com/text2audio?lan=zh&ie=UTF-8&text=".urlencode($matches[1]));
 	$file = fopen($_GET['from'].".mp3","w");
 	echo fwrite($file,$mp3);
 	fclose($file);
