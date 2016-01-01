419fdfdffa14d117ee27dce9bf81b8a37a413602
dennydai <i@daihan.cn>
2016-01-01 22:41:23 +0800

    delete some useless plugins

--- plugins/tts.php
+++
@@ -1,8 +0,0 @@
-<?php
-PluginSet("tts", "Text To Speech");
-	$mp3 = file_get_contents("http://tts.baidu.com/text2audio?lan=zh&ie=UTF-8&text=".urlencode($plugin_text));
-	$file = fopen($from.".mp3","w");
-	echo fwrite($file,$mp3);
-	fclose($file);
-	$BOT->send_audio($from, "/home/wwwroot/1.vps.dennx.com/".$from.".mp3");
-	//unlink($matches[1].".mp3");
