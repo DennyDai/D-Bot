634b0390ab33816e8e246d14f8dde1f15b0c78b9
dennydai <i@daihan.cn>
2016-01-01 22:56:39 +0800

    remove outdated class, and merge functions file

--- QUERY_BOT.php
+++ QUERY_BOT.php
@@ -2,38 +2,40 @@
 //init
 $Pre_Type = $_SERVER['QUERY_STRING'];
 require_once('config.php');
-require_once('functions.php');
 $plugins = array();
 
 require(PATH.'classes'.DIRECTORY_SEPARATOR.TYPE.DIRECTORY_SEPARATOR.'Base.php');
 require_once(PATH.'classes'.DIRECTORY_SEPARATOR.TYPE.DIRECTORY_SEPARATOR.'init.php');
 
 //load plugins
-
-    $load_plugins = glob(PATH.'plugins'.DIRECTORY_SEPARATOR.'*.php');
-    foreach ($load_plugins as $key => $value) {
-        if ($value == PATH.'plugins'.DIRECTORY_SEPARATOR.'help.php') {
-            unset($load_plugins[$key]);
+$load_plugins = glob(PATH.'plugins'.DIRECTORY_SEPARATOR.'*.php');
+foreach ($load_plugins as $key => $value) {
+    if ($value == PATH.'plugins'.DIRECTORY_SEPARATOR.'help.php') {
+        unset($load_plugins[$key]);
+    }
+}
+foreach ($TAGs as $TAG) {
+    foreach ($load_plugins as $value) {
+        $plugin_name = substr($value, strlen(PATH.'plugins'.DIRECTORY_SEPARATOR), -4);
+        if (preg_match("/^(".preg_quote($TAG, '/').$plugin_name."|".preg_quote($TAG, '/').$plugin_name.preg_quote('@').BOT_NAME.") (.*)$/", $text, $matches)) {
+            $plugin_text = $matches[2];
+            $plugin_sendto = $from;
+            require_once $value;
+            break;
+        }elseif (preg_match("/^(".preg_quote($TAG, '/').$plugin_name.")|(".preg_quote($TAG, '/').$plugin_name.preg_quote('@').BOT_NAME.")$/", $text)) {
+            $plugin_text = $matches[2];
+            $plugin_sendto = $from;
+            require_once $value;
+            break;
         }
     }
-    foreach ($TAGs as $TAG) {
-        foreach ($load_plugins as $value) {
-            $plugin_name = substr($value, strlen(PATH.'plugins'.DIRECTORY_SEPARATOR), -4);
-            if (preg_match("/^(".preg_quote($TAG, '/').$plugin_name."|".preg_quote($TAG, '/').$plugin_name.preg_quote('@').BOT_NAME.") (.*)$/", $text, $matches)) {
-                $plugin_text = $matches[2];
-                $plugin_sendto = $from;
-                require_once $value;
-                break;
-            }elseif (preg_match("/^(".preg_quote($TAG, '/').$plugin_name.")|(".preg_quote($TAG, '/').$plugin_name.preg_quote('@').BOT_NAME.")$/", $text)) {
-                $plugin_text = $matches[2];
-                $plugin_sendto = $from;
-                require_once $value;
-                break;
-            }
-        }
-    }
-    require_once PATH.'plugins'.DIRECTORY_SEPARATOR.'help.php';
+}
+require_once PATH.'plugins'.DIRECTORY_SEPARATOR.'help.php';
 
-
+//function
+function PluginSet($desc){
+        global $plugins,$plugin_name,$TAG;
+        $plugins[] .= $TAG.$plugin_name." ".$desc;
+}
 
 ?>
