e6c12df5c2df30cee36f04395093633e26326628
dennydai <i@daihan.cn>
2016-01-07 08:08:33 +0800

    Order Change

--- config.php
+++ config.php
@@ -12,9 +12,9 @@ $TAGs = array('#','/','!');
 define('TB_TOKEN', 'YOUR_TELEGRAM_BOT_TOKEN_HERE');
 define('TB_API_URL', 'https://api.telegram.org/bot'.TB_TOKEN.'/'); //DO NOT CHANGE THIS
 
+//For Wechat-MP
+define('WC_UserName', 'YOUR_WECHAT_USERNAME_HERE');
+
 //For plugins
 define('PLUGIN_GITHUB_TOKEN', 'YOUR_GITHUB_TOKEN_HERE');
 define('PLUGIN_STEAM_KEY', 'YOUR_STEAM_KEY_HERE');
-
-//For Wechat-MP
-define('WC_UserName', 'YOUR_WECHAT_USERNAME_HERE');
