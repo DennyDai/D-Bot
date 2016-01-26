5a6b8483a46a749ac6b0dae06ab0cf92cf728481
dennydai <i@daihan.cn>
2016-01-26 12:36:53 +0800

    minor bug fix

--- VAR_BOT.php
+++ VAR_BOT.php
@@ -20,12 +20,20 @@ foreach ($TAGs as $TAG) {
         if (preg_match("/^(".preg_quote($TAG, '/').$plugin_name."|".preg_quote($TAG, '/').$plugin_name.preg_quote('@').BOT_NAME.") (.*)$/", $text, $matches)) {
             $plugin_text = $matches[2];
             $plugin_sendto = $from;
-            require_once $value;
+            if (!empty($plugin_text)){
+                require_once $value;
+            }else{
+                $BOT->msg($plugin_sendto, "Missing argument(s)");
+            }   
             break;
         }elseif (preg_match("/^(".preg_quote($TAG, '/').$plugin_name.")|(".preg_quote($TAG, '/').$plugin_name.preg_quote('@').BOT_NAME.")$/", $text)) {
             $plugin_text = $matches[2];
             $plugin_sendto = $from;
-            require_once $value;
+            if (!empty($plugin_text)){
+                require_once $value;
+            }else{
+                $BOT->msg($plugin_sendto, "Missing argument(s)");
+            }   
             break;
         }
     }
